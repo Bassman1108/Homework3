1517e29fd0812482fdcb2cc375e0fa7f7d184418
Chad Simmons <Chad.A.Simmons-1@ou.edu>
2019-02-27 10:31:34 -0600

    made class

---
+++ WpfApp1/WpfApp1/Address.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Address
+    {
+        public int StreetNumber { get; set; }
+        public string StreetName { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public int ZipCode { get; set; }
+
+        public Address()
+        {
+            StreetNumber = 0;
+            StreetName = "";
+            State = "";
+            City = "";
+            ZipCode = 0;
+
+        }
+
+        public Address(int streetNumber, string streetName, string state, string city, int zipCode)
+        { streetNumber = StreetNumber;
+            streetName = StreetName;
+            State = state;
+            City = city;
+            ZipCode = zipCode;
+        }
+    }
+}
